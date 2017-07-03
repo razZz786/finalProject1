e624717e001c7f3ea57cdf90580a1c1cd5ebc6f4
razZz786 <bunnyg003@gmail.com>
2017-07-02 23:54:24 -0400

    rajat

--- app/src/main/java/com/example/hacker/myapplication/divide.java
+++ app/src/main/java/com/example/hacker/myapplication/divide.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class divide extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_divide);
     }
-    public void divide (View view)
+    public void comp (View view)
     {
-        Intent intent=new Intent(this,divide.class);
+        Intent intent=new Intent(this,companyLogin.class);
         startActivity(intent);
     }
 }
